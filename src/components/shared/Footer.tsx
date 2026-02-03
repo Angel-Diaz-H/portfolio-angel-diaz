bc36011c309206424e7abf5c3698ebc6e2f1ea79
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-02-03 17:25:32 -0600

    Contenido de Footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,15 @@
 export const Footer = () => {
   return (
-    <footer className="bg-primary p-3">
-      <p className="text-primary-foreground text-center">
-        Proyecto realizado con React, Tailwind CSS y ShadCN.
+    <footer className="py-8 text-center text-sm">
+      <p>Diseñado y desarrollado por Angel Díaz.</p>
+      <p>
+        Proyecto realizado con
+        <span className="text-primary font-semibold"> TypeScript</span>,
+        <span className="text-primary font-semibold"> React</span>,
+        <span className="text-primary font-semibold"> Tailwind CSS</span>,
+        <span className="text-primary font-semibold"> ShadCN</span>,
+        <span className="text-primary font-semibold"> Vite</span> y
+        <span className="text-primary font-semibold"> Next.js</span>.
       </p>
     </footer>
   );
