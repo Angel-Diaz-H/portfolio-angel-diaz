58a88035553190a4750c8e46d54fcb7ba9a961b3
Angel Díaz <angeldiaz.hrn@gmail.com>
2026-02-10 15:10:35 -0600

    Componentes Shared

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,15 +1,14 @@
 export const Footer = () => {
   return (
-    <footer className="py-8 text-center text-sm">
+    <footer className="pb-8 text-center text-sm">
       <p>Diseñado y desarrollado por Angel Díaz.</p>
       <p>
         Proyecto realizado con
         <span className="text-primary font-semibold"> TypeScript</span>,
         <span className="text-primary font-semibold"> React</span>,
         <span className="text-primary font-semibold"> Tailwind CSS</span>,
-        <span className="text-primary font-semibold"> ShadCN</span>,
         <span className="text-primary font-semibold"> Vite</span> y
-        <span className="text-primary font-semibold"> Next.js</span>.
+        <span className="text-primary font-semibold"> ShadCN</span>.
       </p>
     </footer>
   );
